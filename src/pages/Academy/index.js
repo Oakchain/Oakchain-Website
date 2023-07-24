122dd1e3f94f098ff610bb2c8eb934a1b13ba2cc
f-deniyi <holarfemilekan049@gmail.com>
2023-07-24 03:18:36 -0700

    academy: video courses

--- src/pages/Academy/index.js
+++ src/pages/Academy/index.js
@@ -1,19 +1,11 @@
 import React, { useState } from 'react'
 import AuthGuard from "../../Layout/AuthGuard";
 import '../../assets/style/academy.css'
-import academyBg from '../../assets/images/academyBg.svg'
-import { arrayGenerator } from '../../utils';
-import Article from '../../component/Academy/Article';
-import art1 from '../../assets/images/art1.svg'
-import art2 from '../../assets/images/art2.svg'
-import art3 from '../../assets/images/art3.svg'
-import { Row, Col } from 'reactstrap'
+import ArticleCourses from './Article';
+import VideoCourses from './Video';
 
 const Academy = () => {
     const [activeTab, setActiveTab] = useState('article')
-    const academyTag = ['All', 'Active', 'In progress', 'Completed']
-    const [activeTag, setActiveTag] = useState('All');
-    const images = [art1, art2, art3];
 
     return (
         <AuthGuard>
@@ -42,53 +34,10 @@ const Academy = () => {
                     }}
                     className={`academy-tab__item mb-0 text-center w-100 ${activeTab === 'video' ? 'active' : ""}`} role='button'>Video Courses</p>
             </div>
-            <div className='d-flex align-items-center justify-content-between oak-container-fluid oak-mb'>
-                <div>
-                    <h5 className="oak-section__title w-100">Read and Learn about Blockchain and Crypto</h5>
-                </div>
-                <div>
-                    <div className="d-flex align-items-center" >
-                        {
-                            academyTag.map(el => <p
-                                role='button'
-                                onClick={() => {
-                                    setActiveTag(el)
-                                }}
-                                className={`me-1 oak-news_tag text-center flex-shrink-0 mb-0 ${activeTag === el ? 'active' : ""}`}>
-                                {el}
-                            </p>
-                            )
-                        }
-                    </div>
-                </div>
 
-
-
-            </div>
-            <div className='oak-container-fluid'>
-                <Row className='g-3 mb-3'>
-                    {arrayGenerator(3).map((el, i) =>
-                        <Col sm='12' md='4'>
-                            <Article image={images[i]} id={i+1} />
-                        </Col>
-                    )}
-                </Row>
-                <Row className='g-3 mb-3'>
-                    {arrayGenerator(3).map((el, i) =>
-                        <Col sm='12' md='4'>
-                            <Article image={images[i]} id={i+1} />
-                        </Col>
-                    )}
-                </Row>
-                <Row className='g-3 mb-3'>
-                    {arrayGenerator(3).map((el, i) =>
-                        <Col sm='12' md='4'>
-                            <Article image={images[i]} id={i+1} />
-                        </Col>
-                    )}
-                </Row>
-
-            </div>
+            {
+                activeTab === 'article' ? <ArticleCourses /> : <VideoCourses />
+            }
 
         </AuthGuard>
     )
