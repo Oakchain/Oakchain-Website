d9d945664237c32d7164ef432b077aa69f48263a
lucidwrld <kabiawubenedict@gmail.com>
2023-08-23 23:28:14 +0100

    Profile Update

--- src/component/oakwallet/index.js
+++ src/component/oakwallet/index.js
@@ -19,7 +19,7 @@ const OakWallet = ({ isWOpen, toggle }) => {
 
   return (
     <Modal isOpen={isWOpen} toggle={toggle} className='modal-dialog-centered' modalClassName='oak-modal__card'>
-      <div className='Top'>
+      <section className='desktopView'><div className='Top'>
         <div className='accountTopp'>
           <h3>Oak Wallet</h3>
           <button onClick={toggle}>
@@ -37,49 +37,112 @@ const OakWallet = ({ isWOpen, toggle }) => {
           </div>
         </div>
       </div>
-      <div className="navbar">
-        <Nav className='nab' tabs>
-          <NavItem className='tab'>
-            <NavLink
-              className={classnames({ active: activeTab === '1' })}
-              onClick={() => toggleTab('1')}
-            >
-              Transactions
-            </NavLink>
-          </NavItem>
-          <NavItem className='tab'>
-            <NavLink
-              className={classnames({ active: activeTab === '2' })}
-              onClick={() => toggleTab('2')}
-            >
-              Tokens
-            </NavLink>
-          </NavItem>
-          <NavItem className='tab'>
-            <NavLink
-              className={classnames({ active: activeTab === '3' })}
-              onClick={() => toggleTab('3')}
-            >
-              NFTs
-            </NavLink>
-          </NavItem>
-        </Nav>
-        <div className="active-bar" style={{ left: `${(parseInt(activeTab) - 1) * 33.3333}%` }}></div>
+        <div className="navbar">
+          <Nav className='nab' tabs>
+            <NavItem className='tab'>
+              <NavLink
+                className={classnames({ active: activeTab === '1' })}
+                onClick={() => toggleTab('1')}
+              >
+                Transactions
+              </NavLink>
+            </NavItem>
+            <NavItem className='tab'>
+              <NavLink
+                className={classnames({ active: activeTab === '2' })}
+                onClick={() => toggleTab('2')}
+              >
+                Tokens
+              </NavLink>
+            </NavItem>
+            <NavItem className='tab'>
+              <NavLink
+                className={classnames({ active: activeTab === '3' })}
+                onClick={() => toggleTab('3')}
+              >
+                NFTs
+              </NavLink>
+            </NavItem>
+          </Nav>
+          <div className="active-bar" style={{ left: `${(parseInt(activeTab) - 1) * 33.3333}%` }}></div>
+        </div>
+        <TabContent activeTab={activeTab}>
+          <TabPane className='detailTab' tabId='1'>
+            {<div className='trans'>
+              <h3>No Recent Transactions</h3></div>}
+          </TabPane>
+          <TabPane className='detailTab' tabId='2'>
+            {<div className='tokens'>
+              <h3>No Tokens</h3></div>}
+          </TabPane>
+          <TabPane className='detailTab' tabId='3'>
+            {<div className='nft'>
+              <h3>Oops... No NFTs yet</h3></div>}
+          </TabPane>
+        </TabContent></section>
+
+      <section className='mobileView'><div className='Top'>
+        <div className='accountTopp'>
+          <h3>Oak Wallet</h3>
+          <button onClick={toggle}>
+            <img src={fatimess} alt='times' />
+          </button>
+        </div>
+        <div className='oakC'><img src={oak} alt='oak'></img><h3>Oak Nuts</h3></div>
+        <h3 className='amounT'>11.4563 </h3>
+        <div className='buttonss'>
+          <div className='btnHolder'>
+            <div><button><img src={plus} alt='buy'></img></button> <h3>Buy</h3></div>
+            <div><button><img src={send} alt='send'></img></button> <h3>Send</h3></div>
+            <div><button><img src={swap} alt='swap'></img> </button><h3>Swap</h3></div>
+            <div><button><img src={scan} alt='scan'></img> </button><h3>Scan</h3></div>
+          </div>
+        </div>
       </div>
-      <TabContent activeTab={activeTab}>
-        <TabPane className='detailTab' tabId='1'>
-          {<div className='trans'>
-            <h3>No Recent Transactions</h3></div>}
-        </TabPane>
-        <TabPane className='detailTab' tabId='2'>
-          {<div className='tokens'>
-            <h3>No Tokens</h3></div>}
-        </TabPane>
-        <TabPane className='detailTab' tabId='3'>
-          {<div className='nft'>
-            <h3>Oops... No NFTs yet</h3></div>}
-        </TabPane>
-      </TabContent>
+        <div className="navbar">
+          <Nav className='nab' tabs>
+            <NavItem className='tab'>
+              <NavLink
+                className={classnames({ active: activeTab === '1' })}
+                onClick={() => toggleTab('1')}
+              >
+                Transactions
+              </NavLink>
+            </NavItem>
+            <NavItem className='tab'>
+              <NavLink
+                className={classnames({ active: activeTab === '2' })}
+                onClick={() => toggleTab('2')}
+              >
+                Tokens
+              </NavLink>
+            </NavItem>
+            <NavItem className='tab'>
+              <NavLink
+                className={classnames({ active: activeTab === '3' })}
+                onClick={() => toggleTab('3')}
+              >
+                NFTs
+              </NavLink>
+            </NavItem>
+          </Nav>
+          <div className="active-bar" style={{ left: `${(parseInt(activeTab) - 1) * 33.3333}%` }}></div>
+        </div>
+        <TabContent activeTab={activeTab}>
+          <TabPane className='detailTab' tabId='1'>
+            {<div className='trans'>
+              <h3>No Recent Transactions</h3></div>}
+          </TabPane>
+          <TabPane className='detailTab' tabId='2'>
+            {<div className='tokens'>
+              <h3>No Tokens</h3></div>}
+          </TabPane>
+          <TabPane className='detailTab' tabId='3'>
+            {<div className='nft'>
+              <h3>Oops... No NFTs yet</h3></div>}
+          </TabPane>
+        </TabContent> </section>
+
     </Modal>
   );
 };
