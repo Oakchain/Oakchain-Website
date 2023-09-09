74265386dfa1e14f271eca10e26dfc42fbab168f
lucidwrld <kabiawubenedict@gmail.com>
2023-09-09 09:23:04 +0100

    Benedict's Push

--- src/Layout/Header/index.js
+++ src/Layout/Header/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useContext, useEffect, useRef } from 'react';
 import './index.css'
 import logo from '../../assets/images/logo.svg'
 import oakMenu from '../../assets/images/oak_menu.svg'
@@ -7,15 +7,62 @@ import arrow from '../../assets/images/arrow.svg'
 // import { connectWallet } from '../../utils/walletConnect'
 import WalletModal from '../../component/WalletModal'
 
+import { useNavigate } from "react-router-dom";
+
+
+// Custom hook to handle clicking outside an element
+function useClickOutside(ref, callback) {
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (ref.current && !ref.current.contains(event.target)) {
+                callback();
+            }
+        }
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [ref, callback]);
+}
 
 const Header = ({ toggle }) => {
+
+    const navigate = useNavigate()
+    const [isResourcesDisplayed, setIsResourcesDisplayed] = useState(false);
+
+    const resourcesDropdownRef = useRef(null);
+    const companyDropdownRef = useRef(null);
+
+    // Custom hook to handle clicking outside dropdowns and close them
+    useClickOutside(resourcesDropdownRef, () => {
+        setIsResourcesDisplayed(false);
+    });
+
+    useClickOutside(companyDropdownRef, () => {
+        setIsCompanyDisplayed(false);
+    });
+
+    // Function to toggle the Resources display
+    const toggleResourcesDisplay = () => {
+        console.log("working")
+        setIsResourcesDisplayed((prevState) => !prevState);
+
+    }
+    const [isCompanyDisplayed, setIsCompanyDisplayed] = useState(false);
+
+    // Function to toggle the Company display
+    const toggleCompanyDisplay = () => {
+        console.log("working")
+        setIsCompanyDisplayed((prevState) => !prevState);
+
+    }
+
     const [isOpen, setIsOpen] = useState(false)
 
     const toggleModal = () => {
         setIsOpen(!isOpen)
     }
-
-
     return (
         <div className='  position-fixe w-100'>
             <div className='oak-container-fluid d-flex align-items-center justify-content-between oak-header'>
@@ -25,19 +72,74 @@ const Header = ({ toggle }) => {
 
                 <ul className='d-md-flex d-none align-items-center justify-content-between nav-items__wrapper'>
                     <li className='nav-item'>How it Works</li>
-                    <li className='nav-item d-flex align-items-center'>
+                    <li className='nav-item d-flex align-items-center' onClick={toggleResourcesDisplay}>
                         Resources
                         <span className='dropdown-wrapper'>
                             <img src={dropdown} alt='logo' />
                         </span>
+                        {isResourcesDisplayed && (
+                            <div className='oak-resources' ref={resourcesDropdownRef}>
+                                <ul>
+                                    <li onClick={() => {
+                                        navigate('/Community')
+                                    }}>
+                                        <h3>Community</h3>
+                                    </li>
+                                    <li onClick={() => {
+                                        navigate('/Learn')
+                                    }}>
+                                        <h3>Learn</h3>
+                                    </li>
+                                    <li onClick={() => {
+                                        navigate('/Blog')
+                                    }}>
+                                        <h3>Our Blog</h3>
+                                    </li>
+                                    <li onClick={() => {
+                                        navigate('/Server')
+                                    }}>
+                                        <h3>Discord Server</h3>
+                                    </li>
+                                </ul>
+                            </div>
+                        )}
 
                     </li>
-                    <li className='nav-item d-flex align-items-center'>
+
+
+                    <li className='nav-item d-flex align-items-center' onClick={toggleCompanyDisplay}>
                         Company
 
                         <span className='dropdown-wrapper'>
                             <img src={dropdown} alt='logo' />
                         </span>
+                        {isCompanyDisplayed && (
+                            <div className='oak-company' ref={companyDropdownRef}>
+                                <ul>
+                                    <li onClick={() => {
+                                        navigate('/about')
+                                    }}>
+                                        <h3>About Us</h3>
+                                    </li>
+                                    <li onClick={() => {
+                                        navigate('/team')
+                                    }}>
+                                        <h3>Our Team</h3>
+                                    </li>
+                                    <li onClick={() => {
+                                        navigate('/faqs')
+                                    }}>
+                                        <h3>FAQs</h3>
+                                    </li>
+                                    <li onClick={() => {
+                                        navigate('/help')
+                                    }}>
+                                        <h3>Help Center</h3>
+                                    </li>
+                                </ul>
+                            </div>
+                        )}
+
                     </li>
 
                 </ul>
--- src/Layout/Sidebar/index.js
+++ src/Layout/Sidebar/index.js
@@ -5,9 +5,29 @@ import WalletModal from '../../component/WalletModal'
 import arrow from '../../assets/images/arrow.svg'
 import dropdown from '../../assets/images/dropdown.svg'
 
+import { useNavigate } from "react-router-dom";
 
 
 const Sidebar = ({ show, toggle }) => {
+
+    const navigate = useNavigate()
+    const [isResourcesDisplayed, setIsResourcesDisplayed] = useState(false);
+
+    // Function to toggle the Resources display
+    const toggleResourcesDisplay = () => {
+        console.log("working")
+        setIsResourcesDisplayed((prevState) => !prevState);
+
+    }
+    const [isCompanyDisplayed, setIsCompanyDisplayed] = useState(false);
+
+    // Function to toggle the Company display
+    const toggleCompanyDisplay = () => {
+        console.log("working")
+        setIsCompanyDisplayed((prevState) => !prevState);
+
+    }
+
     const [isOpen, setIsOpen] = useState(false)
 
     const toggleModal = () => {
@@ -33,20 +53,72 @@ const Sidebar = ({ show, toggle }) => {
 
                     <ul className=' nav-items__wrapper'>
                         <li className='nav-item mobile'>How it Works</li>
-                        <li className='nav-item mobile d-flex align-items-center'>
+                        <li className='nav-item mobile d-flex align-items-center' onClick={toggleResourcesDisplay}>
                             Resources
                             <span className='dropdown-wrapper'>
                                 <img src={dropdown} alt='logo' />
                             </span>
 
                         </li>
-                        <li className='nav-item mobile d-flex align-items-center'>
+                        {isResourcesDisplayed && (
+                            <div className='oak-resourcess'>
+                                <ul>
+                                    <li onClick={() => {
+                                        navigate('/Community')
+                                    }}>
+                                        <h3>Community</h3>
+                                    </li>
+                                    <li onClick={() => {
+                                        navigate('/Learn')
+                                    }}>
+                                        <h3>Learn</h3>
+                                    </li>
+                                    <li onClick={() => {
+                                        navigate('/Blog')
+                                    }}>
+                                        <h3>Our Blog</h3>
+                                    </li>
+                                    <li onClick={() => {
+                                        navigate('/Server')
+                                    }}>
+                                        <h3>Discord Server</h3>
+                                    </li>
+                                </ul>
+                            </div>
+                        )}
+                        <li className='nav-item mobile d-flex align-items-center' onClick={toggleCompanyDisplay}>
                             Company
 
                             <span className='dropdown-wrapper'>
                                 <img src={dropdown} alt='logo' />
                             </span>
                         </li>
+                        {isCompanyDisplayed && (
+                            <div className='oak-companyy'>
+                                <ul>
+                                    <li onClick={() => {
+                                        navigate('/about')
+                                    }}>
+                                        <h3>About Us</h3>
+                                    </li>
+                                    <li onClick={() => {
+                                        navigate('/team')
+                                    }}>
+                                        <h3>Our Team</h3>
+                                    </li>
+                                    <li onClick={() => {
+                                        navigate('/faqs')
+                                    }}>
+                                        <h3>FAQs</h3>
+                                    </li>
+                                    <li onClick={() => {
+                                        navigate('/help')
+                                    }}>
+                                        <h3>Help Center</h3>
+                                    </li>
+                                </ul>
+                            </div>
+                        )}
 
                     </ul>
                 </div>
