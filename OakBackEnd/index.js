37a0e6c1dda995bb30053c2b4b7e1559177e1a32
Osemu <67153387+osemuaimiosior@users.noreply.github.com>
2023-11-01 20:56:36 +0100

    Add files via upload

--- OakBackEnd/index.js
+++ OakBackEnd/index.js
@@ -2,12 +2,26 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const path = require('path');
-const fs = require('fs');
-const fsPromises = require('fs').promises;
-
 const PORT = process.env.PORT || 4300;
 
-app.use(cors());
+const whitelist = ["htttp://www.oakchain.io", 'http://127.0.0.1:5500']
+const corsOptions = {
+    origin: (origin, callback) =>{
+        if(whitelist.indexOf(origin) != -1 || !orgin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+}
 
+app.use(cors(corsOptions));
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', (req,res) =>{
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
 
 app.listen(PORT, () => console.log('Server is running on port', PORT));
