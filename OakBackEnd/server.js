a3a11fe3932b3d896aaecab5ec69ca0800727aaf
Osemu <67153387+osemuaimiosior@users.noreply.github.com>
2023-11-09 09:35:10 +0100

    Update server.js

--- OakBackEnd/server.js
+++ OakBackEnd/server.js
@@ -19,7 +19,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', (req,res) =>{
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
