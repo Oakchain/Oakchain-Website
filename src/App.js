db4661f8a1b7878d46e7e389f7fc6da3906d0a4e
James Osunkiyesi <jaydhales@gmail.com>
2024-01-16 23:48:18 +0100

    Switched Web3 Integration from wagmi and web3modal to ThirdWeb

--- src/App.js
+++ src/App.js
@@ -1,52 +1,69 @@
-import Router from './router'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './assets/style/index.css'
-import './assets/font/css/index.css'
-import { createContext, useState } from 'react';
-import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
-import { Web3Modal } from '@web3modal/react'
-import { configureChains, createConfig, WagmiConfig } from 'wagmi'
-import { arbitrum, mainnet, polygon } from 'wagmi/chains'
-export const ThemeContext = createContext(null)
+import Router from "./router";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/style/index.css";
+import "./assets/font/css/index.css";
+import { createContext, useState } from "react";
+// import {
+//   EthereumClient,
+//   w3mConnectors,
+//   w3mProvider,
+// } from "@web3modal/ethereum";
+// import { Web3Modal } from "@web3modal/react";
+// import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { Arbitrum, Ethereum, Polygon } from "@thirdweb-dev/chains";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+export const ThemeContext = createContext(null);
 
-
-const chains = [arbitrum, mainnet, polygon]
-const projectId = '5211f08b0f1c65f5ac9a9d5b4a6b3a65'
-const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors: w3mConnectors({ projectId, chains }),
-  publicClient
-})
-const ethereumClient = new EthereumClient(wagmiConfig, chains)
+// const chains = [arbitrum, mainnet, polygon];
+// const projectId = "5211f08b0f1c65f5ac9a9d5b4a6b3a65";
+// const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
+// const wagmiConfig = createConfig({
+//   autoConnect: true,
+//   connectors: w3mConnectors({ projectId, chains }),
+//   publicClient,
+// });
+// const ethereumClient = new EthereumClient(wagmiConfig, chains);
 
 function App() {
-  const [theme, setTheme] = useState("light")
+  const [theme, setTheme] = useState("light");
   const toggler = () => {
-    setTheme((curr) => (curr === "light" ? "dark" : "light"))
-  }
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
 
   return (
     <ThemeContext.Provider value={{ theme, toggler }}>
       <div id={theme}>
-        <WagmiConfig config={wagmiConfig}>
+        <ThirdwebProvider
+          supportedChains={[Arbitrum, Ethereum, Polygon]}
+          clientId="f834ca0ad370cc60524088a7152429eb"
+        >
           <Router />
-        </WagmiConfig>
-
-        <Web3Modal
-          projectId={projectId}
-          ethereumClient={ethereumClient}
-          themeVariables={{
-            '--w3m-overlay-background-color': 'rgba(0,0,0,0.5)',
-            '--w3m-overlay-backdrop-filter': "blur(20px)",
-            '--w3m-logo-image-url': 'https://res.cloudinary.com/dmgydkvnt/image/upload/v1694268719/logo_qo4eoh.png',
-            '--w3m-background-color': '#fff'
-          }}
-        />
+        </ThirdwebProvider>
       </div>
     </ThemeContext.Provider>
-
   );
+
+  // return (
+  //   <ThemeContext.Provider value={{ theme, toggler }}>
+  //     <div id={theme}>
+  //       <WagmiConfig config={wagmiConfig}>
+  //         <Router />
+  //       </WagmiConfig>
+
+  //       <Web3Modal
+  //         projectId={projectId}
+  //         ethereumClient={ethereumClient}
+  //         themeVariables={{
+  //           "--w3m-overlay-background-color": "rgba(0,0,0,0.5)",
+  //           "--w3m-overlay-backdrop-filter": "blur(20px)",
+  //           "--w3m-logo-image-url":
+  //             "https://res.cloudinary.com/dmgydkvnt/image/upload/v1694268719/logo_qo4eoh.png",
+  //           "--w3m-background-color": "#fff",
+  //         }}
+  //       />
+  //     </div>
+  //   </ThemeContext.Provider>
+  // );
 }
 
 export default App;
