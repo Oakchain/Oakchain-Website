1eb90dac6a6ffe57c215766402d0fbaee534e568
Samuel Dahunsi <dahunsisamuel1st@gmail.com>
2024-03-02 13:27:02 +0000

    update

--- src/component/PostModal/index.js
+++ src/component/PostModal/index.js
@@ -1,5 +1,9 @@
 import { Modal } from "reactstrap";
 import "./index.css";
+import gallery from "../../assets/images/gallery.png";
+import face from "../../assets/images/face.png";
+import gif from "../../assets/images/gif.png";
+import tag from "../../assets/images/tag.png";
 import { RxCross2 } from "react-icons/rx";
 
 const PostModal = ({toggle, isOpen, setIsOpen}) => {
@@ -11,11 +15,28 @@ const PostModal = ({toggle, isOpen, setIsOpen}) => {
     // modalClassName="oak-modal__card"
   >
     <div>
-        <div>
-            <p>P</p>
-            <p>Patrick James</p>
+        <div className="top">
+          <div className="name">
+              <p className="initial">P</p>
+              <p>Patrick James</p>
+          </div>
+          <RxCross2/>
         </div>
-        <RxCross2 />
+        <textarea placeholder="What's happening..."/>
+        <div>
+          <div>
+            <div><img src={gallery} alt="gallery"></img></div>
+            <div><img src={face} alt="face"></img></div>
+            <div><img src={gif} alt="gif"></img></div>
+            <div>
+              <div>
+              <img src={tag} alt="tag"></img>
+              <p>E.g Bitcoin</p>
+              </div>
+            </div>
+          </div>
+          <button>Push</button>
+        </div>
     </div>
   </Modal>
   )
