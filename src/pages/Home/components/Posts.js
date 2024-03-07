5b95d91154fad8ee2ce7c3da463f1ea3b1da514a
James Osunkiyesi <jaydhales@gmail.com>
2024-03-07 01:05:35 +0100

    Post Integration

--- src/pages/Home/components/Posts.js
+++ src/pages/Home/components/Posts.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./style.css";
 import Post from "./Post";
 import PostList from "./PostList";
+import axios from "axios";
 
-const postData = [
+const _postData = [
   { id: 1, poster: "DegenPoet", type: "Post", content: "At Oak, our mission is to facilitate the adoption of Web3 technology by providing accessible education, fostering skill development, and bridging the gap between talent and opportunity. We believe that a well-rounded, empowered and diverse community is essential for the sustainable growth of the Web3 ecosystem..." },
   { id: 2, poster: "DegenPoet", type: "Article", content: "At Oak, our mission is to facilitate the adoption of Web3 technology by providing accessible education, fostering skill development, and bridging the gap between talent and opportunity. We believe that a well-rounded, empowered and diverse community is essential for the sustainable growth of the Web3 ecosystem..." },
   { id: 3, poster: "DegenPoet", type: "Post", content: "At Oak, our mission is to facilitate the adoption of Web3 technology by providing accessible education, fostering skill development, and bridging the gap between talent and opportunity. We believe that a well-rounded, empowered and diverse community is essential for the sustainable growth of the Web3 ecosystem..." },
@@ -18,7 +19,30 @@ const postData = [
   { id: 6, poster: "DegenPoet", type: "Post", content: "At Oak, our mission is to facilitate the adoption of Web3 technology by providing accessible education, fostering skill development, and bridging the gap between talent and opportunity. We believe that a well-rounded, empowered and diverse community is essential for the sustainable growth of the Web3 ecosystem..." }
 ];
 
+const oakBaseUrl = "https://api.oakchain.io";
+const accessToken = localStorage.getItem('token');
+
 const Posts = ({view}) => {
+  const [postData, setPostData] = useState([]);
+
+  const fetchPosts = async () => {
+    try {
+      const response = await axios.get(`${oakBaseUrl}/api/blog`);
+      const res = response.data;
+      const allPosts = res.data.blogPosts;
+
+      setPostData(allPosts);
+
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+      // setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
   return (
     <div className="posts">
       {postData.map(post => (
