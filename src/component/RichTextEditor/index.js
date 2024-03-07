20ece056ba5f6badd685f7ea2e9cd50e85899cba
James Osunkiyesi <jaydhales@gmail.com>
2024-03-07 01:22:40 +0100

    Added emoji support

--- src/component/RichTextEditor/index.js
+++ src/component/RichTextEditor/index.js
@@ -1,16 +1,29 @@
 import React, { useState } from "react";
-import ReactQuill from "react-quill";
+import ReactQuill, {Quill} from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
 import "./index.css";
+import quillEmoji from "react-quill-emoji";
+import "react-quill-emoji/dist/quill-emoji.css";
 
+Quill.register(
+    {
+      "formats/emoji": quillEmoji.EmojiBlot,
+      "modules/emoji-toolbar": quillEmoji.ToolbarEmoji,
+      "modules/emoji-textarea": quillEmoji.TextAreaEmoji,
+      "modules/emoji-shortname": quillEmoji.ShortNameEmoji
+    },
+    true
+);
 const RichTextEditor = ({ convertedText, setConvertedText }) => {
   const quillModules = {
     toolbar: [
       [{ header: [2, 3, false] }],
-      ["bold", "italic", "underline", "strike"],
+      ["bold", "italic", "underline", "strike", 'emoji'],
       ["image", "video", "link"],
+
     ],
+    "emoji-toolbar": true,
   };
   return (
     <div className="RichTextEditor">
